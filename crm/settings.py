5a6bf12991016b5b474e1d1de39dd4e05ca79061
Starck43 <saloon.as@gmail.com>
2024-01-18 22:31:07 +0300

    comment print

--- crm/settings.py
+++ crm/settings.py
@@ -105,7 +105,7 @@ CACHES = {
     'default': env.cache()
 }
 
-print(CACHES['default']['LOCATION'])
+# print(CACHES['default']['LOCATION'])
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
